9e27ac6b0cf8e1a206b142531f9cde1c267c05ba
mksudhanshu17 <mksudhanshu17@outlook.com>
2026-08-13 23:25:07 +0530

    User entity initial commit

--- src/main/java/com/system/smartParking/entity/User.java
+++
@@ -1,4 +0,0 @@
-package com.system.smartParking.entity;
-
-public class User {
-}
---
+++ src/main/java/com/system/smartParking/user/entity/User.java
@@ -0,0 +1,14 @@
+package com.system.smartParking.user.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private String phone;
+}
